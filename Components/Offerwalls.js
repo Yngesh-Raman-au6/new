48e1269e799375a6c1f93d0fe1674d18863c5eb9
sid86-dev <sid86harth@gmail.com>
2022-05-22 22:40:35 +0530

    offerwall model wrap

--- Components/Offerwalls.js
+++ Components/Offerwalls.js
@@ -1,25 +1,45 @@
-import NewWindow from 'react-new-window'
-
+import { useState } from 'react';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 const Offerwalls = ({ offers }) => {
 
-    const openTab = (url) => {
-        var height = 570;
-        var width = 520;
-        var left = (screen.width / 2) - (width / 2);
-        var top = (screen.height / 2) - (height / 2);
-        window.open(url, '_blank',
-            `location=yes,directories=no,resizable=yes,height=${height},toolbar=no,menubar=no,width=${width},scrollbars=yes,status=no,top=${top}, left=${left}`)
+    const[offerwallTitle, setOfferwallTitle] = useState('')
+    const[offerwallUrl, setOfferwallUrl] = useState('')
+
+    const openTab = (url, offerwallTitle) => {
+        setOfferwallTitle(offerwallTitle);
+        setOfferwallUrl(url);
+        window.open(url, "theFrame");
     }
 
     return (
 
         <div className="p-5">
+
+            <div className="modal fade" id="offerWallModel" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div className="modal-content ">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">
+                                {offerwallTitle} 
+                                <a href={offerwallUrl} rel="noreferrer"
+                                    target="_blank" ><OpenInNewIcon className="mb-1 mx-1" fontSize="small" />
+                                </a>
+                            </h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <iframe className="modal-body mx-auto" name="theFrame"  height="570" width="100%"></iframe>
+                       
+                    </div>
+                </div>
+            </div>
+
             <div className="row row-cols-1 g-4 pb-5">
 
                 <div className="col-lg-3">
-                    <a onClick={() => openTab('https://wall.adgaterewards.com/oKuUpw/471109')}
-                         className="card p-3 text-decoration-none">
+                    <a onClick={() => openTab('https://wall.adgaterewards.com/oKuUpw/471109', 'Adgate Media')}
+                        className="card p-3 text-decoration-none" style={{ cursor: "pointer" }} data-bs-toggle="modal" data-bs-target="#offerWallModel">
+
                         <div className="card bg-dark text-white">
                             <img src="https://s3-eu-west-1.amazonaws.com/tpd/logos/5bddd30afbd6140001fa4ffb/0x0.png" className="card-img" alt="..." />
                             <div className="card-img-overlay">
@@ -36,8 +56,12 @@ const Offerwalls = ({ offers }) => {
 
                 </div>
                 <div className="col-lg-3">
-                    <a onClick={() => openTab('https://fastrsrvr.com/list/471109')}
+
+                    <a style={{ cursor: "pointer" }} onClick={() => openTab('https://fastrsrvr.com/list/471109', 'CPA Leads')}
+                        data-bs-toggle="modal" data-bs-target="#offerWallModel"
                         className="card p-3 h-100 text-decoration-none">
+
+
                         <div className="card bg-dark text-white">
                             <img src="https://images-na.ssl-images-amazon.com/images/I/61onWbZBZSL.png"
                                 className="card-img" alt="..." />
