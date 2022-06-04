f184bcb5d2a25a5bd712f7bbac1d16a53759b8a2
Siddhartha Roy <sid86harth@gmail.com>
2022-06-04 09:54:14 +0000

    bug fix

---
+++ pages/api/postback/adgate.js
@@ -0,0 +1,29 @@
+import dbConnect from "../../../utils/dbConnect";
+import userSchema from '../../../utils/models/userSchema'
+
+dbConnect();
+
+export default async function handler(req, res) {
+
+    if (req.method !== 'POST') {
+        return res.status(500).json({ message: 'Soory this is post route' })
+    }
+
+    const { user_id, point_value } = req.query;
+
+    // get user data
+    const userData = await userSchema.findOne({
+        _id: user_id
+    });
+
+    if (!userData) {
+        return res.json({ success: false, message: 'Something went wrong' });
+    };
+
+    // update the coin value
+    const prevCoins = parseInt(userData.coins)
+    userData.coins = prevCoins + parseInt(point_value);
+    await userData.save();
+
+    res.status(200).json(req.query)
+}
---
+++ pages/api/postback/cpa.js
@@ -0,0 +1,31 @@
+import dbConnect from "../../../utils/dbConnect";
+import userSchema from '../../../utils/models/userSchema'
+
+dbConnect();
+
+export default async function handler(req, res) {
+
+    if (req.method !== 'POST') {
+        return res.status(500).json({ message: 'Soory this is post route' })
+    }
+
+    console.log(req.body)
+
+    const { subid, payout } = req.query;
+
+    // get user data
+    const userData = await userSchema.findOne({
+        _id: subid
+    });
+
+    if (!userData) {
+        return res.json({ success: false, message: 'Something went wrong' });
+    };
+
+    // update the coin value
+    const prevCoins = parseInt(userData.coins)
+    userData.coins = prevCoins + parseInt(payout);
+    await userData.save();
+
+    return res.status(200).json({ success: true })
+}
